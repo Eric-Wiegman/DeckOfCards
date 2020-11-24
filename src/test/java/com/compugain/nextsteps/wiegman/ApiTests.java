a96529ad7d15389b34da884fc17089006dcc1159
Eric Wiegman <eric_wiegman@hotmail.com>
2020-11-24 12:02:46 -0800

    Adding more test cases

--- src/test/java/com/compugain/nextsteps/wiegman/ApiTests.java
+++ src/test/java/com/compugain/nextsteps/wiegman/ApiTests.java
@@ -1,10 +1,13 @@
 package com.compugain.nextsteps.wiegman;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.compugain.nextsteps.wiegman.CardsAPI.CardItem;
 
@@ -31,6 +34,7 @@ public class ApiTests
     			 "The remaining # of cards is not what was expected.");
     }
     
+    
     /**
      * DataDriven HttpGet Tests for exact cards picked -- unshuffled
      * new deck without Jokers
@@ -54,4 +58,149 @@ public class ApiTests
     	 Assert.assertEquals(actual, expected, 
     			 "The actual cards are not what were expected.");
     }
+    
+    /**
+     * DataDriven HttpGet Tests for counting total cards after shuffling
+     * <<deck>> number of new decks, without Jokers
+     * @throws IOException 
+     */
+    @Test (dataProvider = "data-provider-get-cards-muti-decks", 
+    		dataProviderClass = DataProviderClass.class )
+    public void getCardsInMultipleDecksTests(
+    		int decks,
+    		int expected) throws IOException
+    {
+    	
+    	String apiUrl = 
+    			CardsTester.BASE_URL +
+    			"/new/shuffle/?deck_count=" +
+    			decks;
+    	
+    	JSONObject json = new JSONObject();
+		try {
+			Fetch.GETRequest("GET", 
+					apiUrl,
+					"");
+			json = JsonHandler.convertResponseToJsonObject (
+					HTTPRequest.getResponseBody());
+		} catch (IOException e) {
+			//Ignore this exception. We handle this next.
+		}
+		
+		int totalCards = json.getInt("remaining");
+    	
+    	 
+    	 Assert.assertEquals(totalCards, expected, 
+    			 "The actual cards are not what were expected.");
+    }
+    
+    /**
+     * DataDriven HttpGet Tests that verify the file name of the image
+     * is equal to the code associated with the Card.
+     * @throws IOException 
+     */
+    @Test ( )
+    public void imageHasCodeNameTests() throws IOException
+    {
+    	SoftAssert softAssert = new SoftAssert();
+    	
+    	int counter = 0;
+    	while (counter < 5) {
+    		CardsAPI.createNewDeckOfCards(true);
+    		CardsAPI.shuffleDeck(CardsAPI.getDeckId());
+
+    		List<CardItem> list = CardsAPI.drawCards(1);
+    		String filePath = "";
+    		String code = "";
+    		for (CardsAPI.CardItem cards : list) {
+    			filePath = cards.getImage();
+    			code = cards.getCode();
+    		}
+
+    		File f = new File(filePath);
+    		String fileName = f.getName().replace(".png", "");
+
+    		System.out.println("file name of " + fileName + " should equal " 
+    				+ "code of " + code);
+    		
+    		softAssert.assertEquals(fileName, code, 
+    				"The file name of the image is not equal to the code for the Card.");
+    		
+    		counter++;
+
+    }
+    	softAssert.assertAll();
+    	
+    	}
+    
+    //reshuffle of same deck should not change the deckId
+    /**
+     * DataDriven HttpGet Tests for verifying that the deckId does
+     * not change when you reshuffle the same deck of cards.
+     * @throws IOException 
+     */
+    @Test ()
+    public void reshuffleDoesNotChangeDeckIdTests() throws IOException
+    {
+    	
+    	SoftAssert softAssert = new SoftAssert();
+    	
+    	int counter = 0;
+    	while (counter < 5) {
+    	
+    	CardsAPI.createNewDeckOfCards(true);
+    	String deckId = CardsAPI.getDeckId();
+    	CardsAPI.shuffleDeck(deckId);
+    	String shuffledDeckId = CardsAPI.getDeckId();
+    	
+    	System.out.println("Original deck ID of " + deckId + 
+    			" should remain the same as " + shuffledDeckId +
+    			" when the same Deck is reshuffled.");
+    	 
+    	 softAssert.assertEquals(deckId, shuffledDeckId,
+    			 "Reshuffle of same deck of cards changed the Deck ID.");
+    	 
+    	 counter++;
+    	}
+    	
+    	softAssert.assertAll();
+    }
+    
+    
+    /**
+     * DataDriven HttpGet Tests for verifying the shuffle state.
+     * @throws IOException 
+     */
+    @Test (dataProvider = "data-provider-shuffling-test", 
+    		dataProviderClass = DataProviderClass.class )
+    public void cardShufflingTests(
+    		boolean doShuffle,
+    		boolean expected) throws IOException
+    {
+    	boolean isShuffled = false;
+    	
+    	String apiUrl = CardsTester.BASE_URL + "/new/";
+    	
+    	JSONObject json = new JSONObject();
+		try {
+			Fetch.GETRequest("GET", 
+					apiUrl,
+					"");
+			json = JsonHandler.convertResponseToJsonObject (
+					HTTPRequest.getResponseBody());
+		} catch (IOException e) {
+			//Ignore this exception. We handle this next.
+		}
+		
+		isShuffled = json.getBoolean("shuffled");
+		CardsAPI.setDeckId(json.getString("deck_id"));
+    	
+    	if (doShuffle) {
+    		isShuffled = CardsAPI.shuffleDeck(CardsAPI.getDeckId());
+    	}
+    	 
+    	 Assert.assertEquals(doShuffle, isShuffled, 
+    			 "The shuffle state of the deck of cards is not what was expected.");
+    }
+    
 }
--- src/test/java/com/compugain/nextsteps/wiegman/DataProviderClass.java
+++ src/test/java/com/compugain/nextsteps/wiegman/DataProviderClass.java
@@ -28,6 +28,8 @@ public class DataProviderClass
         	{54,0}
         };
     }
+    
+    
     /**
      * We pick X cards and expect exact picks (value and suit) as in this 
      * case we do not add Jokers and do not shuffle the deck
@@ -43,6 +45,44 @@ public class DataProviderClass
 			{13, "ACE SPADES 2 SPADES 3 SPADES 4 SPADES 5 SPADES 6 SPADES "
 					+ "7 SPADES 8 SPADES 9 SPADES 10 SPADES JACK SPADES "
 					+ "QUEEN SPADES KING SPADES"}
+        };
+    }
+    
+    
+    /**
+     * We pick X cards and expect exact picks (value and suit) as in this 
+     * case we do not add Jokers
+     * @return ObjectArray that contains the data used to drive the tests
+     */
+    @DataProvider(name = "data-provider-get-cards-muti-decks")
+    public static Object[][] dataProviderGetCardCountForMultiDecks()
+    {
+        return new Object[][] { 
+        	//{decks,expected}
+			{1,52},
+			{3, 156},
+			{13, 676},
+			{0,0},
+			//A negative # of decks is rejected, and we have zero cards
+			{-1,0}
+			
+        };
+    }
+    
+    
+    /**
+     * We are checking if we shuffle the cards that the shuffle state is true
+     * otherwise it is false
+     * @return ObjectArray that contains the data used to drive the tests
+     */
+    @DataProvider(name = "data-provider-shuffling-test")
+    public static Object[][] dataProviderGetShuffleStateForDec()
+    {
+        return new Object[][] { 
+        	//{doShuffle,expected}
+			{true,true},
+			{false,false}
+			
         };
     }
     
